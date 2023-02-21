24b2bbb7ecde9f6b819b771c0e07b637445bdf12
aasuncion-fol <113270642+aasuncion-fol@users.noreply.github.com>
2023-02-21 14:54:26 -0500

    resolve conflicts between brenn and master

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import LoginForm from './components/LoginForm/LoginForm';
-import SignUpForm from './components/SignUpForm/SignUpForm';
-
 import LayoutPage from "./pages/common/LayoutPage";
 import LoginPage from "./pages/login/LoginPage";
 import ResetPasswordPage from "./pages/reset-password/ResetPasswordPage";
@@ -10,16 +7,16 @@ import SignupPage from "./pages/signup/SignupPage";
 
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <h1>Start Point</h1>
-        <LoginForm />
-      </div>
-      <div>
-        <SignUpForm />
-      </div>
-    </div>
+  return ( <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LayoutPage />}>
+        <Route index element={<LoginPage />} />
+        <Route path="reset" element={<ResetPasswordPage />} />
+        <Route path="signup" element={<SignupPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+    
   );
 }
 
