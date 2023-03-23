fa2917ca6b4719c8359eac049a75b154e3feff79
aasuncion-fol <113270642+aasuncion-fol@users.noreply.github.com>
2023-03-22 22:50:40 -0400

    remove unused func

--- src/pages/Listings/AddListingPage.js
+++ src/pages/Listings/AddListingPage.js
@@ -6,18 +6,6 @@ import './ListingPage.css'
 import Alert from '@mui/material/Alert'
 
 
-
-const Item = styled(Paper)(({ theme }) => ({
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-  boxShadow:'none',
-  borderRadius: 'none',
-  minHeight: '80vh',
-  border: '1px solid #e3e3e3'
-}));
-
-
 const AddListingPage = () => {
   
   return(
